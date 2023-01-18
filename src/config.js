95fb8819c89493b0dadc2275f61758a09939c71a
hamzamaan <hamza37811@gmail.com>
2023-01-18 09:33:52 +0000

    updated pool id and host

--- src/config.js
+++ src/config.js
@@ -13,9 +13,9 @@ export const appConfig = {
   initStateAPI: 'https://dljlgj9y26.execute-api.us-east-2.amazonaws.com/Prod/InitState/', // e.g. https://12abcdef89.execute-api.us-west-2.amazonaws.com/Prod/InitState/
   // MODULE 2 - REALTIME
   iot: {
-    poolId: '', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
-    host: '', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
-    region: '' // e.g. 'us-west-1'
+    poolId: 'us-east-2:4337f7f0-ef17-405b-a424-b784218f8fbb', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
+    host: 'a2t3x2tfa69fgl-ats.iot.us-east-2.amazonaws.com', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
+    region: 'us-east-2' // e.g. 'us-west-1'
   },
   // MODULE 3 - PHOTOS
   photoUploadURL: '', // e.g. https://12abcdfg89.execute-api.us-west-2.amazonaws.com/Prod/Upload
